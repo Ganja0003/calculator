b7cf4bcecd3597f2c4e90c2a0988734b8101b0af
Yuusuf Roble <yuuusuf98@outlook.com>
2023-12-15 19:47:18 +0100

    i have added a liitle javascript to dislpay the number on the console but will continue with the project tommorow Insha Allah

---
+++ script.js
@@ -0,0 +1,23 @@
+let operator = "";
+let previousValue = "";
+let currentValue  = "";
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clear = document.querySelector(".clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let CurrentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+    }))
+
+function handleNumber(num){
+    console.log(num);
+}
+})
